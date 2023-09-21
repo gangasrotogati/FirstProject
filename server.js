24c22e69fbcc69ae670f12bc9b01f98459028620
Joe Glass <glass_joseph@hotmail.com>
2023-09-20 20:55:11 -0700

    first commit (sample page)
    first commit

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 // Create a basic HTTP server
 const server = http.createServer((req, res) => {
   // Set the response header
-  res.setHeader('Content-Type', 'text/plain');
-  
-  // Send a "Hello, World!" response
-  res.end('Hello, World!\n');
+  res.setHeader('Content-Type', 'text/html');
+
+  // Define the directory where your HTML files are located
+  const directoryPath = path.join(__dirname, 'public');
+
+  // Determine the requested URL path
+  let filePath = path.join(directoryPath, req.url === '/' ? 'index.html' : req.url);
+
+  // Check if the file exists
+  fs.exists(filePath, (exists) => {
+    if (exists) {
+      // Read the file and send it as the response
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+        } else {
+          res.statusCode = 200;
+          res.end(data);
+        }
+      });
+    } else {
+      // If the file does not exist, send a 404 Not Found response
+      res.statusCode = 404;
+      res.end('File Not Found');
+    }
+  });
 });
 
 const port = 3000;
